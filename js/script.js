493b960fbadf20149610653067560a638753a6c8
Jeferson <93160555+Jeferson-Oliveiraa@users.noreply.github.com>
2024-05-10 20:00:15 -0300

    Atualizando código

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+/* Extensor Theme para as cores */
 const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+const accordionHeades = document.querySelectorAll(".accordion-header")
+const menuLinks = document.querySelectorAll(".menu-linl")
 
+function changeTheme () {
+    const currentTheme = rootHtml.getAttribute("data-theme");
 
+}
